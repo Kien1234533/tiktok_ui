2f4a1555059aa3e4e3738aed711b98347edf3ce5
Kien1234533 <dacnguyen732003@gmail.com>
2024-08-01 23:40:38 +0700

    Cài đặt babel plugin module resolver | Dự án Tiktok Clone với ReactJS

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import myAlert from '~/myAlert'
+
+const Button = () => {
+  return (
+    <div>
+        <button onClick={myAlert}> Click me !</button>
+       
+        </div>
+  )
+}
+
+export default Button
---
+++ src/myAlert.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const myAlert = () => {
+  return (
+    <div>
+        {alert("Say hi !")}
+    </div>
+  )
+}
+
+export default myAlert
